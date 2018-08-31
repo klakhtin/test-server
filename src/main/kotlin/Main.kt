d204850161105de94f18d206db1632c784891bb3
klakhtin <kirill.lakhtin@jetbrains.com>
2018-08-31 15:14:12 +0700

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.tomcat.Tomcat
+
+fun main(args: Array<String>) {
+    embeddedServer(Tomcat, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+}
